9b4f5da92407a763b5dfa6003b809fad96dc6c36
OM Miles <miles@ommiles.com>
2021-05-17 19:16:22 -0700

    commit tree 9cd9100b8c3bcb09f01fe3ede3ac782b42875740

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line(line)
+    # if line.size == 0
+    if line.empty?
+        puts "The line is currently empty."
+    else
+        current_line = "The line is currently:"
+        line.each.with_index(1) {|person, i| current_line << " #{i}. #{person}"}
+        puts current_line
+    end
+end
+
+def take_a_number(line, new_customer)
+    # line.push(new_customer)
+    line << new_customer
+    puts "Welcome, #{new_customer}. You are number #{line.size} in line."
+end
+
+def now_serving(line)
+    # if line.size == 0
+    if line.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{line.shift}."
+    end
+end
